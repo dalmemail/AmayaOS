0869c02064c0340319e39d9d6611158155fa02dc
Dan Rulos <dalmemail@gmail.com>
2015-07-04 11:08:55 +0200

    AmayaOS: Update wc

--- bin/wc/Main.cpp
+++ bin/wc/Main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Dan Rulos.
+ * Copyright (C) 2015 Dan Rulos
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,18 +20,90 @@
 #include <files.h>
 #include <stdlib.h>
 
+#define IN 0
+#define OUT 1
+
+#define YES 0
+#define NO 1
+
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-      printf("Uso: wc archivo\r\n");
-      return EXIT_FAILURE;
-    }
-    file *f = new file();
-    f->setpath(argv[1]);
-    f->f_open(O_RDONLY);
-    char *ch = f->readAll();
-    f->f_close();
-    printf("%d", strlen(ch));
-    printf("\r\n");
-    return EXIT_SUCCESS;
+	int c,l,w = NO;
+	if (argc < 2) {
+		printf("Uso: %s FILE1 FILE2...\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	/* check arguments */
+	int no = 0;
+	for (int i=1; i < argc; i++) {
+		if (strcmp(argv[i], "-c") == 0) {
+			c = YES;
+			no++;
+		}
+		if (strcmp(argv[i], "-l") == 0) {
+			l = YES;
+			no++;
+		}
+		if (strcmp(argv[i], "-w") == 0) {
+			w = YES;
+			no++;
+		}
+	}
+	if (no == 0) {
+		c = l = w = YES;
+	}
+	int sc,sl,sw = 0;
+	int nc,nl,nw = 0;
+	int state = OUT;
+	for (int i=1; i < argc; i++) {
+		if (strcmp(argv[i], "-c") == 0 ||strcmp(argv[i], "-w") == 0 ||strcmp(argv[i], "-l") == 0) {
+		}
+		else {
+			file *f = new file();
+			f->setpath(argv[i]);
+			f->f_open(O_RDONLY);
+			char *ch = f->readAll();
+			f->f_close();
+			nc = nl = nw = 0;
+			for (unsigned int k=0; k < strlen(ch); k++) {
+				nc++;
+				if (ch[k] == '\n') {
+					nl++;
+				}
+				if (ch[k] == '\n' || ch[k] == ' ' || ch[k] == '\t') {
+					state = OUT;
+				}
+				else if (state == OUT) {
+					state = IN;
+					nw++;
+				}
+			}
+			if (l == YES) {
+				printf("\t%d", nl);
+			}
+			if (w == YES) {
+				printf("\t%d", nw);
+			}
+			if (c == YES) {
+				printf("\t%d", nc);
+			}
+			printf("\t%s\n", argv[i]);
+			sl = sl + nl;
+			sw = sw + nw;
+			sc = sc + nc;
+		}
+	}
+	if (argc - no > 2) {
+		if (l == YES) {
+			printf("\t%d", sl);
+		}
+		if (w == YES) {
+			printf("\t%d", sw);
+		}
+		if (c == YES) {
+			printf("\t%d", sc);
+		}
+		printf("\ttotal\n");
+	}
+	return EXIT_SUCCESS;
 }
