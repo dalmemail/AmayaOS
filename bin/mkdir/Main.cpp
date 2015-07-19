1c2db6189befd1bb29ead1ba3c4e044a029dcd74
Dan Rulos <dalmemail@gmail.com>
2015-07-19 10:35:20 +0200

    AmayaOS: Coreutils v0.4.8-mkdir

--- bin/mkdir/Main.cpp
+++ bin/mkdir/Main.cpp
@@ -1,6 +1,6 @@
 /* AMAYA OS 2014 */
 /*
- * Copyright (C) 2014 Dan Rulos
+ * Copyright (C) 2015 Dan Rulos
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <sys/types.h>
+
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,25 +23,40 @@
 #include <string.h>
 #include <unistd.h>
 
+int mkfolder(char *prog, char *path)
+{
+	char *final_path;
+	/* if there isn't a path */
+	if (path[0] != '/') {
+		final_path = new char [128];
+		getcwd(final_path, sizeof(final_path));
+		if ((strcmp(final_path, "/")) != 0) {
+			strcat(final_path, "/");
+		}
+		strcat(final_path, path);
+	}
+	else {
+		final_path = new char [strlen(path)];
+		strcpy(final_path, path);
+	}
+	if ((mkdir(final_path, S_IRUSR | S_IWUSR)) < 0) {
+		printf("%s: error al abrir '%s': %s\r\n",
+                prog, path, strerror(errno));
+		return EXIT_FAILURE;
+	}
+	delete final_path;
+	return EXIT_SUCCESS;
+}
+
 int main(int argc, char **argv)
 {
-    int i;
-    char cwd[128];
-    /* Obtenemos los argumentos de la linea de comandos */
-    if(argc < 2) { /* Si hay menos de 2 avisa al usuario */
-        printf("uso: %s DIRECTORIO\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    for (i=1; argv[i]; i++) {
-      if (argv[i][0]!='/') { /* Necesito una ruta... */
-        getcwd(cwd, sizeof(cwd));
-        strcat(cwd, "/");
-        strcat(cwd, argv[i]);
-        mkdir(cwd, S_IWUSR | S_IRUSR);
-      }
-      else {
-        mkdir(argv[i], S_IWUSR | S_IRUSR);
-      }
-    }
-    return EXIT_SUCCESS; /* Todo ha salido bien */
+	int i, ret = EXIT_SUCCESS;
+	if (argc < 2) {
+		printf("uso: %s DIRECTORIO1 DIRECTORIO2 ...\n", argv[0]);
+		return EXIT_SUCCESS;
+	}
+	for (i = 1; i < argc; i++) {
+		ret = mkfolder(argv[0], argv[i]);
+	}
+	return ret;
 }
