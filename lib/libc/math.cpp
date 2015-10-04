a3136c25229072e7f050514e2bb70fae1bc265c0
Dan Rulos <dalmemail@gmail.com>
2015-10-04 12:37:13 +0200

    AmayaOS: AmayaMath

--- lib/libc/math.cpp
+++ lib/libc/math.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015 Alvaro Stagg
- * 
+ * Copyright (C) 2015 Alvaro Stagg [alvarostagg@openmailbox.org]
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,129 +17,499 @@
 
 #include "math.h"
 
-int pow(int base, int potencia)
+double pow(double base, double potencia)
 {
-	int m = 1;
+	if(potencia == 0.0 && base != 0.0)
+		return 1.0;
+	else if(potencia == 0.0 && base == 0.0)
+		return 0.0;
 
-	for (int i = 1; i <= potencia; i++)
+	double m = 1.0;
+
+	for(int i = 1; i <= potencia; i++)
 	{
-		m *= base;
+  	m *= base;
+	}
+
+  return m;
+}
+
+float pow(float base, float potencia)
+{
+	if(potencia == 0.0 && base != 0.0)
+		return 1.0;
+	else if(potencia == 0.0 && base == 0.0)
+		return 0.0;
+
+	float m = 1.0;
+
+	for (int i = 1;i <= potencia; i++)
+	{
+  	m *= base;
 	}
 
 	return m;
 }
 
-double pow(double base, double potencia)
+long double pow(long double base, long double potencia)
 {
-	double m = 1;
+	if(potencia == 0.0 && base != 0.0)
+		return 1.0;
+	else if(potencia == 0.0 && base == 0.0)
+		return 0.0;
 
-  	for(int i = 1; i <= potencia; i++)
-  	{
-    		m *= base;
-  	}
+	long double m = 1.0;
 
-  	return m;
+	for(int i = 1; i <= potencia; i++)
+	{
+  	m *= base;
+	}
+
+	return m;
 }
 
-int sqrt(int base)
+double pow(double base, int potencia)
 {
-	int c = 0, mod = 1;
-	
-	if(base <= 0)
-	{
+	if(potencia == 0 && base != 0)
+		return 1;
+	else if(potencia == 0 && base == 0)
 		return 0;
+
+	double m = 1.0;
+
+  for(int i = 1; i <= potencia; i++)
+  {
+  	m *= base;
 	}
-	
-	for(int i = 0; i < 50; c += mod, i++)
-	{
-		if(c * c > base)
-		{
-			c -= mod;
-			mod /= 10;	
-		}
+
+	return m;
+}
+
+long double pow(long double base, int potencia)
+{
+	if(potencia == 0 && base != 0)
+		return 1;
+	else if(potencia == 0 && base == 0)
+		return 0;
+
+	long double m = 1.0;
+
+  for(int i = 1; i <= potencia; i++)
+  {
+  	m *= base;
 	}
-	
-	return c;
+
+	return m;
 }
 
 double sqrt(double base)
 {
-    double c = 0, mod = 1;
-	
+	double c = 0, mod = 1;
+
 	if(base <= 0)
 	{
 		return 0;
 	}
-	
-	for(int i = 0; i < 50; c += mod, i++)
+
+	for(int i = 0; i < (50+base); c += mod, i++)
 	{
 		if(c * c > base)
 		{
 			c -= mod;
-			mod /= 10;	
+			mod /= 10;
 		}
 	}
-	
+
 	return c;
-	
 }
 
-int cbrt(int base)
+float sqrt(float base)
 {
-    int c = 0, mod = 1;
-  
-    if(base <= 0)
-    {
-    	return 0;
-    }
-  
-    for(int i = 0; i < 50; c += mod, i++)
-    {
-    	if(c * c * c > base)
-        {
-    		c -= mod;
-            mod /= 10;
-        }
-    }
-  
-    return c;	
+	float c = 0, mod = 1;
+
+	if(base <= 0)
+	{
+		return 0;
+	}
+
+	for(int i = 0; i < (50+base); c += mod, i++)
+	{
+		if(c * c > base)
+		{
+			c -= mod;
+			mod /= 10;
+		}
+	}
+
+	return c;
+}
+
+long double sqrt(long double base)
+{
+	long double c = 0, mod = 1;
+
+	if(base <= 0)
+	{
+		return 0;
+	}
+
+	for(int i = 0; i < (50+base); c += mod, i++)
+	{
+		if(c * c > base)
+		{
+			c -= mod;
+			mod /= 10;
+		}
+	}
+
+	return c;
 }
 
 double cbrt(double base)
 {
     double c = 0, mod = 1;
-  
+
     if(base <= 0)
     {
     	return 0;
     }
-  
-    for(int i = 0; i < 50; c += mod, i++)
+
+    for(int i = 0; i < base; c += mod, i++)
     {
-    	if(c * c * c > base)
-        {
-          	c -= mod;
-            mod /= 10;
-        }
+			if(c * c * c > base)
+      {
+      	c -= mod;
+        mod /= 10;
+      }
     }
-  
-    return c;	
+
+    return c;
 }
 
-int hypot(int x, int y)
+float cbrt(float base)
 {
-  int rs;
+    float c = 0, mod = 1;
 
-  rs = pow(x, 2) + pow(y, 2);
-  
-  return rs;
+    if(base <= 0)
+    {
+    	return 0;
+    }
+
+    for(int i = 0; i < base; c += mod, i++)
+    {
+    	if(c * c * c > base)
+      {
+      	c -= mod;
+        mod /= 10;
+      }
+    }
+
+    return c;
+}
+
+long double cbrt(long double base)
+{
+	long double c = 0, mod = 1;
+
+	if(base <= 0)
+	{
+		return 0;
+	}
+
+	for(int i = 0; i < base; c += mod, i++)
+	{
+		if(c * c * c > base)
+			{
+					c -= mod;
+					mod /= 10;
+			}
+	}
+
+	return c;
 }
 
 double hypot(double x, double y)
 {
-  double rs;
+	return pow(x, 2) + pow(y, 2);
+}
 
-  rs = pow(x, 2.0) + pow(y, 2.0);
+float hypot(float x, float y)
+{
+	return pow(x, 2) + pow(y, 2);
+}
 
-  return rs;
+long double hypot(long double x, long double y)
+{
+	return pow(x, 2) + pow(y, 2);
+}
+
+double exp2(double x)
+{
+	return pow(2, x);
+}
+
+float exp2(float x)
+{
+	return pow(2, x);
+}
+
+long double exp2(long double x)
+{
+	return pow(2, x);
+}
+
+double log10(double x)
+{
+	int i = 0;
+
+	if(x < 0) /* ERROR */
+		return -1;
+	else
+	{
+		double sn = 0;
+
+		while(sn != x)
+		{
+			if(i >= (int)x)
+				break;
+
+			sn = pow(10.0, i);
+			i++;
+		}
+	}
+
+	return i - 1;
+}
+
+float log10(float x)
+{
+	int i = 0;
+
+	if(x < 0) /* ERROR */
+		return -1;
+	else
+	{
+		float sn = 0;
+
+		while(sn != x)
+		{
+			if(i >= (int)x)
+				break;
+
+			sn = pow(10.0, i);
+			i++;
+		}
+	}
+
+	return i - 1;
+}
+
+long double log10(long double x)
+{
+	int i = 0;
+
+	if(x < 0) /* ERROR */
+		return -1;
+	else
+	{
+		long double sn = 0;
+
+		while(sn != x)
+		{
+			if(i >= (int)x)
+				break;
+
+			sn = pow(10.0, i);
+			i++;
+		}
+	}
+
+	return i - 1;
+}
+
+
+double sin(double x)
+{
+	return x - ((x * x) / (1 * 2 * 3));
+}
+
+float sin(float x)
+{
+	return x - ((x * x) / (1 * 2 * 3));
+}
+
+long double sin(long double x)
+{
+	return x - ((x * x) / (1 * 2 * 3));
+}
+
+double abs(double x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+float abs(float x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+long double abs(long double x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+double fabs(double x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+float fabs(float x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+long double fabs(long double x)
+{
+	if(x < 0)
+		return (x - x) -(x);
+	else
+		return x;
+}
+
+double fma(double x, double y, double z)
+{
+	return (x * y) + z;
+}
+
+float fmaf(float x, float y, float z)
+{
+	return (x * y) + z;
+}
+
+long double fmal(long double x, long double y, long double z)
+{
+	return (x * y) + z;
+}
+
+double fmax(double x, double y)
+{
+	if(x > y)
+		return x;
+	else
+		return y;
+}
+
+float fmaxf(float x, float y)
+{
+	if(x > y)
+		return x;
+	else
+		return y;
+}
+
+long double fmaxl(long double x, long double y)
+{
+	if(x > y)
+		return x;
+	else
+		return y;
+}
+
+double fmin(double x, double y)
+{
+	if(x < y)
+		return x;
+	else
+		return y;
+}
+
+float fminf(float x, float y)
+{
+	if(x < y)
+		return x;
+	else
+		return y;
+}
+
+long double fminl(long double x, long double y)
+{
+	if(x < y)
+		return x;
+	else
+		return y;
+}
+
+double modf(double x, double* intpart)
+{
+	double dec_part = x - (int)x;
+	double _intpart = intpart - &dec_part;
+
+	return dec_part;
+}
+
+float modf(float x, float* intpart)
+{
+	float dec_part = x - (int)x;
+	float _intpart = intpart - &dec_part;
+
+	return dec_part;
+}
+
+long double modf(long double x, long double* intpart)
+{
+	long double dec_part = x - (int)x;
+	long double _intpart = intpart - &dec_part;
+
+	return dec_part;
+}
+
+double floor(double x)
+{
+	double rgv;
+
+	if(x < 0)
+		rgv = (int)x - 1;
+	else
+		rgv = (int)x;
+
+	return rgv;
+}
+
+float floor(float x)
+{
+	float rgv;
+
+	if(x < 0)
+		rgv = (int)x - 1;
+	else
+		rgv = (int)x;
+
+	return rgv;
+}
+
+long double floor(long double x)
+{
+	long double rgv;
+
+	if(x < 0)
+		rgv = (int)x - 1;
+	else
+		rgv = (int)x;
+
+	return rgv;
 }
--- lib/libc/math.h
+++ lib/libc/math.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015 Alvaro Stagg
- * 
+ * Copyright (C) 2015 Alvaro Stagg [alvarostagg@openmailbox.org]
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,27 +14,94 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #ifndef AMAYAMATH_H
 #define AMAYAMATH_H
 
-/* You can get more functions at <http://wiki.amayaos.com/index.php/AmayaMath>. */
-
-#define M_PI 3.14159265358979323846 /* PI */
-#define PI   3.14159265             /* PI min */
+/* algunas constantes que pueden ser utiles */
+#define M_PI      3.14159265358979323846  /* PI */
+#define PI        3.14159265              /* PI min */
+#define M_PI_2    1.57079632679489661923  /* pi / 2 */
+#define M_PI_4    0.78539816339744830962  /* pi / 4 */
+#define M_1_PI    0.31830988618379067154  /*  1 / PI */
+#define M_2_PI    0.63661977236758134308  /*  2 / PI */
 
 /* Funciones de potenciación */
-int pow(int base, int potencia);
 double pow(double base, double potencia);
+float pow(float base, float potencia);
+long double pow(long double base, long double potencia);
+double pow(double base, int potencia);
+long double pow(long double base, int potencia);
 
-int sqrt(int base);
 double sqrt(double base);
+float sqrt(float base);
+long double sqrt(long double base);
 
-int cbrt(int base);
 double cbrt(double base);
+float cbrt(float base);
+long double cbrt(long double base);
 
-int hypot(int x, int y);
 double hypot(double x, double y);
+float hypot(float x, float y);
+long double hypot(long double x, long double y);
+
+double exp2(double x);
+float exp2(float x);
+long double exp2(long double x);
 /* Fin de funciones de potenciación */
 
-#endif
+/* trigonometría
+double cos(double x);
+float cos(float x);
+long double cos(long double x);
+
+double sin(double x);
+float sin(float x);
+long double sin(long double x);
+
+*** AÚN NO ESTA EN USO ***
+***  STILL NOT IN USE  ***
+
+ fin trigonometría */
+
+/* Logaritmos */
+double log(double x);
+float log(float x);
+long double log(long double x);
+
+double log10(double x);
+float log10(float x);
+long double log10(long double x);
+/* Fin logaritmos */
+
+/* Otras */
+double abs(double x);
+float abs(float x);
+long double abs(long double x);
+
+double fabs(double x);
+float fabs(float x);
+long double fabs(long double x);
+
+double fma(double x, double y, double z);
+float fmaf(float x, float y, float z);
+long double fmal(long double x, long double y, long double z);
+
+double fmax(double x, double y);
+float fmaxf(float x, float y);
+long double fmaxl(long double x, long double y);
+
+double fmin(double x, double y);
+float fminf(float x, float y);
+long double fminl(long double x, long double y);
+
+double modf(double x, double* intpart);
+float modf(float x, float* intpart);
+long double modf(long double x, long double* intpart);
+
+double floor(double x);
+float floor(float x);
+long double floor(long double x);
+/* Fin otras */
+
+#endif /* AmayaMath.h */
