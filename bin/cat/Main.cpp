a3ebda28fb8ed887d707b3849b7b170773554cd2
Dan Rulos <dalmemail@gmail.com>
2015-12-22 18:11:43 +0100

    Coreutils: v0.5.7: Add --help to /bin/cat/cat and change read files code

--- bin/cat/Main.cpp
+++ bin/cat/Main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Niek Linnenbank
+ * Copyright (C) 2009 Niek Linnenbank, 2015 Dan Rulos
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,10 +18,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <files.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define VERSION "v0.5.7"
+
+int f_size(char *path)
+{
+	struct stat st;
+	int ssize;
+	if ((stat(path, &st)) < 0) {
+		ssize = -1;
+	}
+	else {
+		ssize = st.st_size;
+	}
+	return ssize;
+}
 
 int main(int argc, char **argv)
 {
@@ -29,21 +43,34 @@ int main(int argc, char **argv)
 
     /* verificaamos los argumentos de la linea de comandos. */
     if (argc < 2) {
-        printf("uso: %s FILE1 FILE2 ...\r\n",
+        printf("uso: %s FILE1 FILE2 ...\n",
                 argv[0]);
         return EXIT_FAILURE;
+    }
+    if (argc == 2 && (strcmp(argv[1], "--help")) == 0) {
+	printf("AmayaOS Coreutils %s (C) 2015 AmayaOS Team & Others\n", VERSION);
+	printf("Licencia GNU GPL v3 <http://www.gnu.org/licenses/>.\n");
+	printf("Uso: %s FILE1 FILE2 ...\n", argv[0]);
+	printf("Reportar errores a traves de http://bugs.amayaos.com o amaya@amayaos.com\n");
+	return EXIT_SUCCESS;
     }
     
     /* Obtenemos todos los archivos dados. */
     for (int i = 0; i < argc - 1 ; i++) {
         /* Intentamos mostrar el contenido. */
-        file *f = new file();
-        f->setpath(argv[i + 1]);
-        f->f_open(O_RDONLY);
-        char *ch = f->readAll();
-        f->f_close();
-    
-        printf("%s", ch);
+        int fsize = f_size(argv[i+1]);
+	char *ch = new char [fsize];
+	int fd;
+	if ((fd = open(argv[i+1], O_RDONLY)) < 0) {
+		printf("%s: error al leer '%s'\n", argv[0], argv[i+1]);
+		result = EXIT_FAILURE;
+	}
+	else {
+		read(fd, ch, fsize);
+		close(fd);
+		printf("%s", ch);
+	}
+	delete ch;
 
         /* Actualizamos el resultado si es necesario. */
         if (result > ret)
