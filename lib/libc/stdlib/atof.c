16372c9e323a5ee2a63e0e505508fff3709814de
Dan Rulos <dalmemail@gmail.com>
2016-02-26 17:29:38 +0100

    atof.c: Add negative numbers support

--- lib/libc/stdlib/atof.c
+++ lib/libc/stdlib/atof.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Dan Rulos
+ * Copyright (C) 2016 Dan Rulos
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,6 +35,9 @@ double atof(char *input)
 				div *= 10;
 			}
 		}
+	}
+	if (input[0] == '-') {
+		n *= -1;
 	}
 	return n;
 }
