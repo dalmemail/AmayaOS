d5aa267a116ec4e3921f06b4bb9c19185c04cfdb
Dan Rulos <dalmemail@gmail.com>
2016-04-30 13:46:43 +0200

    AmayaOS: Sleep 0.5.9: Add minutes, hours and days support. By @Algoru

--- bin/sleep/Main.cpp
+++ bin/sleep/Main.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2012 Felipe Cabrera, 2015 Dan Rulos
- * 
+ * Copyright (C) 2012 Felipe Cabrera, 2016 Dan Rulos, 2016 Alvaro Stagg [alvarostagg@openmailbox.org]
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,23 +18,86 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define DIA     86400
+#define HORA    3600
+#define MINUTO  60
+
+static void usage(void);
+static void version(void);
+static int hflag = 0, vflag = 0;
 
 int main(int argc, char **argv)
 {
-    if(argc < 2) {
-        printf("uso: %s SEGUNDOS\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    int s = atoi(argv[1]);
-        
-    if (s >= 0) {
-	sleep(atoi(argv[1]));
+	if(argc < 2)
+	{
+		printf("sleep: falta un operando\n"
+		"Pruebe 'sleep --help' para mas informacion.\n");
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "--help") == 0)
+		{
+ 			usage();
+			hflag = 1;
+			break;
+		}
+		else if (strcmp(argv[i], "--version") == 0)
+		{
+			version();
+			vflag = 1;
+			break;
+		}
+	}
+
+	if (hflag || vflag)
+		return EXIT_SUCCESS;
+	else
+	{
+		if (atoi(argv[1]) >= 0) {
+			switch(argv[1][strlen(argv[1]) - 1])
+			{
+				case 'd':
+					sleep((atoi(&argv[1][0]) * DIA));
+				break;
+				case 'h':
+					sleep((atoi(&argv[1][0]) * HORA));
+				break;
+				case 'm':
+					sleep((atoi(&argv[1][0]) * MINUTO));
+				break;
+				case 's':
+				default:
+					sleep(atoi(&argv[1][0]));
+				break;
+			}
+		}
+	}
+
 	printf("\n");
 
 	return EXIT_SUCCESS;
-    }
-    else {
-	printf("Error: %d\n", s);
-	return EXIT_FAILURE;
-    }
+}
+
+static void version(void)
+{
+	printf("sleep (AmayaOS CoreUtils) 0.5.9\n"
+	"Copyright (C) 2016 AmayaOS, Inc.\n"
+	"Licencia GPLv3+: GPL de GNU version 3 o posterior\n"
+	"<http://gnu.org/licenses/gpl.html>.\n"
+	"Esto es software libre: usted es libre de cambiarlo y redistribuirlo.\n"
+	"No hay NINGUNA GARANTIA, hasta donde permite la ley.\n\n");
+}
+
+static void usage(void)
+{
+	printf("Modo de empleo: sleep NUMERO[SUFIJO]...\n"
+	"        o bien: sleep OPCION\n\n");
+	printf("Hace una pausa de NUMERO segundos. El SUFIJO puede ser 's' para segundos\n"
+	"(predeterminado), 'm' para minutos, 'h' para horas o 'd' para dias.\n\n");
+	printf("      --help     muestra esta ayuda y finaliza\n"
+	"      --version  informa de la version y finaliza\n");
 }
