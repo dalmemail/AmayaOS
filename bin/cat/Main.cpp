b61fa5f8fc2aab6100640a0064d2bcda861591c4
DalmeGNU <dalmemail@gmail.com>
2017-04-08 15:50:45 +0200

    cat 0.6.2: A few update that makes cat a faster util

--- bin/cat/Main.cpp
+++ bin/cat/Main.cpp
@@ -17,6 +17,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
 
@@ -113,19 +115,19 @@ int main(int argc, char* argv[])
 
 static int read_file(int argc, char* fileName)
 {
-    FILE* file = fopen(fileName, "r");
-    if (!file)
+    int file = open(fileName, O_RDONLY);
+    if (file == -1)
         return -1;
 
     int fnsize = get_file_size(fileName);
     char *cnt = (char *)malloc(fnsize);
-    unsigned int n_bytes = fread(cnt, 1, fnsize, file);
+    int n_bytes = read(file, cnt, fnsize);
 
     int lines = 1;
     if (nflag)
         printf("   %d  ", lines);
 
-    for (unsigned int i = 0; i < n_bytes; i++)
+    for (int i = 0; i < n_bytes; i++)
     {
         if (cnt[i] == '\n')
         {
@@ -165,7 +167,7 @@ static int read_file(int argc, char* fileName)
     }
 
     printf("\n");
-    fclose(file);
+    close(file);
     free(cnt);
 
     return 0;
@@ -173,29 +175,29 @@ static int read_file(int argc, char* fileName)
 
 static void usage(void)
 {
-    printf("Modo de empleo: cat [OPCIÓN]... [FICHERO]...\n\n"
-           "  -b, --number-nonblank    No enumera las lineas en blanco, remplaza -n.\n"
+    printf("Modo de empleo: cat [OPCION]... [FICHERO]...\n\n"
+           "  -b, --number-nonblank    No enumera las l%cneas en blanco, remplaza -n.\n"
            "  -e                       Lo mismo que -E.\n"
-           "  -E, --show-ends          Muestra un $ al final de cada linea.\n"
-           "  -n, --number             Enumera cada linea.\n"
+           "  -E, --show-ends          Muestra un $ al final de cada l%cnea.\n"
+           "  -n, --number             Enumera cada l%cnea.\n"
            "  -t                       Lo mismo que -T.\n"
-           "  -T, --show-tabs          Muestra los caracteres de tabulación como ^I\n"
+           "  -T, --show-tabs          Muestra los caracteres de tabulaci%cn como ^I\n"
            "      --help               Muestra esta ayuda y finaliza.\n"
-           "      --version            Informa de la versión y finaliza.\n\n"
+           "      --version            Informa de la versi%cn y finaliza.\n\n"
 
            "AmayaOS Coreutils %s (C) 2017 AmayaOS Team\n"
            "Licencia GNU GPL v3 <http://www.gnu.org/licenses/>.\n"
-           "Reportar errores a través de http://bugs.amayaos.com o alvarostagg@openmailbox.org\n", VERSION);
+           "Reportar errores a trav%cs de amaya@amayaos.com\n", 161, 161, 161, 162, 162, VERSION, 130);
 }
 
 static void version(void)
 {
     printf("cat (AmayaOS Coreutils) v%s\n"
-           "Copyright © 2017 AmayaOS Team.\n"
-           "Licencia GPLv3+: GPL de GNU versión 3 o posterior\n"
+           "Copyright (C) 2017 AmayaOS Team.\n"
+           "Licencia GPLv3+: GPL de GNU versi%cn 3 o posterior\n"
            "<http://gnu.org/licenses/gpl.html>.\n"
            "Esto es software libre: usted es libre de cambiarlo y redistribuirlo.\n"
-           "No hay NINGUNA GARANTÍA, hasta donde permite la ley.\n\n", VERSION);
+           "No hay NINGUNA GARANTIA, hasta donde permite la ley.\n\n", VERSION, 162);
 }
 
 static size_t get_file_size(const char *fileName)
